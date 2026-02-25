e1a75269390a1cfc7144f0f1569549750f539eaa
Slava Fesenko <fvsleader@gmail.com>
2026-02-25 13:01:26 +0200

    +

--- src/modules/ide-to-llm/utils/tech-prompt-builder.ts
+++ src/modules/ide-to-llm/utils/tech-prompt-builder.ts
@@ -2,6 +2,9 @@ import * as vscode from 'vscode';
 
 import type { LlmCopypasterConfig, TechPromptBuilderDetails } from '../../../config';
 
+const TECH_PROMPT_BUILDER_DETAILS_ID_LLM_RESPONSE_RULES = 'llm-response-rules';
+const TECH_PROMPT_BUILDER_DETAILS_ID_WEB_GIT_PROMPT = 'web-git-prompt';
+
 export class BuilderTechPrompt {
   public constructor(
     private readonly _extensionContext: vscode.ExtensionContext,
@@ -11,29 +14,67 @@ export class BuilderTechPrompt {
   public async build(): Promise<string> {
     const techPromptConfig = this._config.techPrompt;
 
-    const promptBuilderDetailsList = techPromptConfig.builders;
-
     const builtPrompts: string[] = [];
 
-    for (const promptBuilderDetails of promptBuilderDetailsList) {
-      if (!promptBuilderDetails.promptConcatenationEnabled) continue;
+    const llmResponseRulesPromptBuilderDetails = this._tryFindTechPromptBuilderDetailsById(
+      techPromptConfig.builders,
+      TECH_PROMPT_BUILDER_DETAILS_ID_LLM_RESPONSE_RULES
+    );
 
-      const promptText = await this._tryReadPromptText(promptBuilderDetails.relativePathToPrompt);
+    const llmResponseRulesPrompt = await this._tryBuildPromptByDetails(llmResponseRulesPromptBuilderDetails, promptText =>
+      this._buildLlmResponseRulesPrompt(promptText, llmResponseRulesPromptBuilderDetails)
+    );
 
-      if (!promptText) continue;
+    if (llmResponseRulesPrompt) builtPrompts.push(llmResponseRulesPrompt);
 
-      const processedPromptText = this._processPromptTextByBuilderHandlerId(promptText, promptBuilderDetails);
+    const webGitPromptBuilderDetails = this._tryFindTechPromptBuilderDetailsById(
+      techPromptConfig.builders,
+      TECH_PROMPT_BUILDER_DETAILS_ID_WEB_GIT_PROMPT
+    );
 
-      if (!processedPromptText.trim()) continue;
+    const webGitPrompt = await this._tryBuildPromptByDetails(webGitPromptBuilderDetails, promptText =>
+      this._buildWebGitPrompt(promptText, webGitPromptBuilderDetails)
+    );
 
-      builtPrompts.push(processedPromptText);
-    }
+    if (webGitPrompt) builtPrompts.push(webGitPrompt);
 
     if (builtPrompts.length === 0) return '';
 
     const delimiterLine = `\n${techPromptConfig.techPromptDelimiter}\n`;
 
     return builtPrompts.join(delimiterLine);
+  }
+
+  private _tryFindTechPromptBuilderDetailsById(
+    techPromptBuilderDetailsList: TechPromptBuilderDetails[],
+    techPromptBuilderDetailsId: string
+  ): TechPromptBuilderDetails | null {
+    const foundTechPromptBuilderDetails = techPromptBuilderDetailsList.find(
+      techPromptBuilderDetails => techPromptBuilderDetails.id === techPromptBuilderDetailsId
+    );
+
+    if (!foundTechPromptBuilderDetails) return null;
+
+    return foundTechPromptBuilderDetails;
+  }
+
+  private async _tryBuildPromptByDetails(
+    techPromptBuilderDetails: TechPromptBuilderDetails | null,
+    buildPromptText: (promptText: string) => string
+  ): Promise<string | null> {
+    if (!techPromptBuilderDetails) return null;
+
+    if (!techPromptBuilderDetails.promptConcatenationEnabled) return null;
+
+    const promptText = await this._tryReadPromptText(techPromptBuilderDetails.relativePathToPrompt);
+
+    if (!promptText) return null;
+
+    const processedPromptText = buildPromptText(promptText);
+
+    if (!processedPromptText.trim()) return null;
+
+    return processedPromptText;
   }
 
   private async _tryReadPromptText(relativePathToPrompt: string): Promise<string | null> {
@@ -48,24 +89,9 @@ export class BuilderTechPrompt {
     }
   }
 
-  private _processPromptTextByBuilderHandlerId(promptText: string, promptBuilderDetails: TechPromptBuilderDetails): string {
-    const promptBuilderHandlerById = this._buildPromptBuilderHandlerById();
+  private _buildLlmResponseRulesPrompt(promptText: string, promptBuilderDetails: TechPromptBuilderDetails | null): string {
+    if (!promptBuilderDetails) return '';
 
-    const promptBuilderHandler = promptBuilderHandlerById[promptBuilderDetails.builderHandlerId];
-
-    if (!promptBuilderHandler) return this._buildGenericPrompt(promptText);
-
-    return promptBuilderHandler(promptText);
-  }
-
-  private _buildPromptBuilderHandlerById(): Record<string, (promptText: string) => string> {
-    return {
-      llmResponseRules: promptText => this._buildLlmResponseRulesPrompt(promptText),
-      webGitPrompt: promptText => this._buildWebGitPrompt(promptText),
-    };
-  }
-
-  private _buildLlmResponseRulesPrompt(promptText: string): string {
     let nextPromptText = promptText;
 
     nextPromptText = this._replacePlaceholdersWithData(
@@ -83,7 +109,9 @@ export class BuilderTechPrompt {
     return nextPromptText;
   }
 
-  private _buildWebGitPrompt(promptText: string): string {
+  private _buildWebGitPrompt(promptText: string, promptBuilderDetails: TechPromptBuilderDetails | null): string {
+    if (!promptBuilderDetails) return '';
+
     let nextPromptText = promptText;
 
     nextPromptText = this._replacePlaceholdersWithData(
