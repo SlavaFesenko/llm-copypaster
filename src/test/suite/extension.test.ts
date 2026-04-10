2f62a1cfe21a093b48380b589fd860f70f266a35
Slava Fesenko <fvsleader@gmail.com>
2026-04-10 12:29:19 +0300

    +

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,3 +1,11 @@
+// ! DO NOT remove this test it's vital for all other tests.
+
+// not sure why, but to make tests displayed in UI, this "import { suite, test } from 'mocha';" should be placed in ONE file
+// but this exact file will be invisible for "UI Test Runner" extension for some magic reason.
+// Tried a couple of workarounds, but all them failed, this is the only stable way, at least for local test run.
+
+// IT'S EXPECTED and acceptable that the exact test above won't be displayed in the "UI Test Runner" extension, but CLI still treats it.
+
 import { suite, test } from 'mocha';
 import assert from 'node:assert/strict';
 import * as vscode from 'vscode';
